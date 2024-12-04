fc01e310bb4beec2f930726c416f2f2e9a901f33
Lipestile <felipebalbino53@gmail.com>
2024-12-04 11:41:28 -0300

    NgClass e NgStyle

--- meu-primeiro-projeto-latest/src/app/components/templates/tamplete-building/tamplete-building.component.ts
+++ meu-primeiro-projeto-latest/src/app/components/templates/tamplete-building/tamplete-building.component.ts
@@ -1,10 +1,11 @@
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgClass, NgStyle } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-tamplete-building',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, NgClass, NgStyle, FormsModule],
   templateUrl: './tamplete-building.component.html',
   styleUrl: './tamplete-building.component.scss'
 })
