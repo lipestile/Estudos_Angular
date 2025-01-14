ebb978bacf07acae93800fa8a3444797d2fb4557
Lipestile <felipebalbino53@gmail.com>
2025-01-13 22:27:26 -0300

    @if APRENDIZADO

--- meu-primeiro-projeto-latest/src/app/app.component.ts
+++ meu-primeiro-projeto-latest/src/app/app.component.ts
@@ -2,17 +2,18 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NewComponent } from './components/new-component/new-component.component';
-import { TampleteBuildingComponent } from './components/templates/tamplete-building/tamplete-building.component';
+import { TampleteBuildingComponent } from './components/template/tamplete-building/tamplete-building.component';
 import { TemplateVariableComponent } from './components/template/template-variable/template-variable.component';
+import { TemplateControlFlowComponent } from './components/template/template-control-flow/template-control-flow.component';
 
 //componentes
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule,RouterOutlet, NewComponent, TampleteBuildingComponent, TemplateVariableComponent],
+  imports: [CommonModule,RouterOutlet, NewComponent, TampleteBuildingComponent, TemplateVariableComponent, TemplateControlFlowComponent],
   template:`
     <h1> Curso Angular</h1>
-    <app-template-variable/>
+    <app-template-control-flow/>
    
   `,
 
